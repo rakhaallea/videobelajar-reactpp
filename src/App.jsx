f23c73f6c42f4a59b674e321089f664cae17bc81
rakhaallea <rakhaallea@gmail.com>
2025-08-09 23:05:46 +0700

    perbaikan struktur tailwindcss

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,25 @@
-import { createBrowserRouter, RouterProvider } from "react-router";
+import { BrowserRouter, Routes, Route } from "react-router";
+import Layout from "./pages/Layout";
 import Homepage from "./pages/Homepage";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <Login />
-  },
-  {
-    path: '/register',
-    element: <Register />
-  },
-  {
-    path: '/home',
-    element: <Homepage />
-  },
-])
-
 const App = () => {
   return (
-    <div>
-      <RouterProvider router={router} />
-    </div>
+    // <div>
+    //   <RouterProvider router={router} />
+    // </div>
+    <BrowserRouter>
+      <Routes>
+        {/* Parent route */}
+        <Route path="/" element={<Layout />}>
+          {/* Child routes */}
+          <Route index element={<Login />} />
+          <Route path="register" element={<Register />} />
+          <Route path="home" element={<Homepage />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   )
 }
 export default App
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,7 @@ import logoNavbar from '../assets/icon/logo_mobile.png';
 
 const Header = () => {
     return (
-        <header className='container py-[16px] px-[24px] border-t-1 border-b-1 border-white shadow-lg'>
+        <header className='@container py-[16px] px-[24px] border-t-1 border-b-1 border-amber-50 shadow-lg'>
             <img src={logoNavbar} alt="video belajar logo" />
         </header>
     )
