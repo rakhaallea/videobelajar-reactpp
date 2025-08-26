a2eaf544fc292f01ca23bade90ae1d3051162a04
rakhaallea <rakhaallea@gmail.com>
2025-08-26 22:36:37 +0700

    init perbaikan code

---
+++ src/utils/dataUser.js
@@ -0,0 +1,9 @@
+const user = [
+    {
+        name: "Allea Rakha",
+        email: "emailpertama@gmail.com",
+        password: "12345678",
+        img: "",
+        phone: "081234567890",
+    },
+];
